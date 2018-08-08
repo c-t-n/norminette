2d6d1238cba55e92d2c62b154306653fbc26c554
Franck LOURME <flourme@e3r1p8.42.fr>
2018-08-09 01:19:40 +0200

    [ADD] Added walk and filtering in multiple paths

--- norminette/cmd.go
+++ norminette/cmd.go
@@ -1,6 +1,7 @@
 package norminette
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/spf13/cobra"
@@ -9,7 +10,6 @@ import (
 type Config struct {
 	Verbose bool
 	Debug   bool
-	Path    string
 	Options Options
 }
 
@@ -17,20 +17,30 @@ type Options struct {
 	BypassPrototypeInCFile bool
 	BypassLineWidth        bool
 	BypassFunctionHeight   bool
+	BypassMakefile         bool
 }
 
 var Cfg Config
 
 var RootCmd = &cobra.Command{
-	Use:   "norminette",
+	Use:   "norminette [flags] [path...]",
 	Short: "Norminette - 42 Norm Linter",
 	Long: `Norminette - 42 Norm Linter
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 A C source code linter builded with love by and for the 42 Community.
 Checks every source file, header file and C Makefile of a specific directory
 and displays every norm errors.`,
+	Args: cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-
+		if len(args) == 0 {
+			fmt.Println("Checking path .")
+			WalkInsideProject(".")
+		} else {
+			for _, path := range args {
+				fmt.Println("Checking path ", path)
+				WalkInsideProject(path)
+			}
+		}
 	},
 }
 
@@ -38,10 +48,10 @@ func init() {
 	RootCmd.AddCommand(versionCmd)
 	RootCmd.PersistentFlags().BoolVarP(&Cfg.Debug, "debug", "d", false, "display debug informations")
 	RootCmd.PersistentFlags().BoolVarP(&Cfg.Verbose, "verbose", "v", false, "set the verbosity level a little higher")
-	RootCmd.Flags().StringVarP(&Cfg.Path, "path", "p", "$PWD", "runs norminette on this path (default: $PWD)")
 	RootCmd.Flags().BoolVarP(&Cfg.Options.BypassPrototypeInCFile, "bypass-prototypes", "", false, "skip errors related to prototypes functions in C files")
 	RootCmd.Flags().BoolVarP(&Cfg.Options.BypassLineWidth, "bypass-line-width", "", false, "skip errors related to line width")
 	RootCmd.Flags().BoolVarP(&Cfg.Options.BypassFunctionHeight, "bypass-function-height", "", false, "skip errors related to the number of lines inside a function block")
+	RootCmd.Flags().BoolVarP(&Cfg.Options.BypassMakefile, "bypass-makefile", "", false, "skip Makefile checks")
 }
 
 func Execute() {
---
+++ norminette/walk.go
@@ -0,0 +1,48 @@
+package norminette
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func WalkInsideProject(path string) {
+	cSourceFile, _ := regexp.Compile(".*\\.c$")
+	hSourceFile, _ := regexp.Compile(".*\\.h$")
+	Makefile, _ := regexp.Compile("^Makefile$")
+
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		//fmt.Printf(" > Checking %q\n", info.Name())
+		if info.IsDir() {
+			//fmt.Printf(" Skipping %q... because it's a DIR\n", path)
+			return nil
+		}
+
+		if cSourceFile.MatchString(info.Name()) {
+			fmt.Printf("cSourceFile\t|  FOUND %q ! NORMINETTING\n", info.Name())
+			// TODO: Add go routine for C file checking
+			return nil
+		}
+
+		if hSourceFile.MatchString(info.Name()) {
+			fmt.Printf("hSourceFile\t|  FOUND %q ! NORMINETTING\n", info.Name())
+			// TODO: Add go routine for H file checking
+			return nil
+		}
+
+		if Makefile.MatchString(info.Name()) && Cfg.Options.BypassMakefile == false {
+			fmt.Printf("Makefile\t|  FOUND %q ! NORMINETTING\n", info.Name())
+			// TODO: Add go routine for Makefile checking
+			return nil
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Something went wrong")
+		fmt.Println(err)
+	}
+
+}
